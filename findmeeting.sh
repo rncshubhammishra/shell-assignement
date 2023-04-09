2b9c3eaa6f2df7f88c98efa8fdd45445f52e0500
rncshubhammishra <71925983+rncshubhammishra@users.noreply.github.com>
2023-04-09 23:30:58 +0530

    Delete findmeeting.sh

--- findmeeting.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash/
-cat agenda.txt | awk ' BEGIN {printf "Day  Agenda \n"} {
-    if ($0 == "Today" || $0 == "Tomorrow") {
-        printf $0 " "
-        getline;
-        printf $0 "\n"
-
-    }
-}'
