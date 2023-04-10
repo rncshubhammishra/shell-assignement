26127dd57fb5abdc045c524e8b142d20fe435077
rncshubhammishra <71925983+rncshubhammishra@users.noreply.github.com>
2023-04-10 16:22:16 +0530

    changed the servername

--- executesqlscript.ps1
+++ executesqlscript.ps1
@@ -1,1 +1,1 @@
-Invoke-Sqlcmd -Query "select * from table Employee" -ServerInstance "MyComputer\MainInstance" -Database "TestDB"
+Invoke-Sqlcmd -Query "select * from table Employee" -ServerInstance "AZR14EUSVDP455" -Database "TestDB"
